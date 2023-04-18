4eb6037813a0a691f1dc4b335148ab7b2f85aa50
yu Chen <chenyu@pybctec.com>
2023-04-18 11:18:58 +0800

    yyy

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,10 @@
-export const tplTypeList = ['web', 'mini-taro', 'mini-uni', 'node'] as const
+export const tplTypeList = [
+  'web',
+  'mini-taro',
+  'mini-uni',
+  'node',
+  'mini-uni-module'
+] as const
 type TplTypeList = typeof tplTypeList[number]
 
 export type ConfigValue = {
